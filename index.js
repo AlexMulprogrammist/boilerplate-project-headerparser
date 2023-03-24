7941478a636347cb5cbef9cc0a4e4f73bde3420b
Oleksander Mul <77461287+AlexMulprogrammist@users.noreply.github.com>
2023-03-24 18:58:52 +0000

    Final project

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.get('/api/whoami', function (req, res) {
   });
 });
 
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
